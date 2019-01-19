4dbf6d07bef6e8325c1963a783b47638cd345687
Hendi Santika <hendisantika@gmail.com>
2019-01-20 05:53:32 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootpostgresql/SpringbootPostgresqlApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPostgresqlApplication.class, args);
+    }
+
+}
+
